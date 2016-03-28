6768b37e11e653abc98bb04cd96451870cfe6415
lincolnmi <1281435925@qq.com>
2016-03-28 19:24:18 +0800

    finish multi rounds of predict

--- src/Runnable/Client.java
+++ src/Runnable/Client.java
@@ -30,6 +30,7 @@ public class Client {
         for (int i=0;i<tag_number;i++) {
             Tag tag = new Tag(Helper.generateID());
             tag.setNum(i+1);
+            tag.setStatus(1);
             tags.add(tag);
         }
         for (Tag tag : tags) {
@@ -56,8 +57,10 @@ public class Client {
         HashMap<Integer,Integer> mappedResults = new HashMap<Integer, Integer>();
         Random rdm = new Random(seed);
         for (Tag tag:tags) {
-            int idx = rdm.nextInt(frame);
-            mappedResults.put(tag.getNum(),idx);
+            if (tag.getStatus()!=0) {
+                int idx = rdm.nextInt(frame);
+                mappedResults.put(tag.getNum(), idx);
+            }
         }
         for (int key:mappedResults.keySet()) {
             System.out.println("Tag "+key + ":" + mappedResults.get(key));
@@ -69,22 +72,24 @@ public class Client {
         System.out.println("map tags");
         HashMap<Integer,Set<Tag>> mappedResults = new HashMap<Integer, Set<Tag>>();
         for (Tag tag:tags) {
-            int idx = mappedIndex.get(tag.getNum());
-            Set<Tag> _tags = null;
-            if (mappedResults.get(idx)==null) {
-                _tags = new HashSet<Tag>();
-            } else {
-                _tags = mappedResults.get(idx);
+            if (tag.getStatus()!=0) {
+                int idx = mappedIndex.get(tag.getNum());
+                Set<Tag> _tags = null;
+                if (mappedResults.get(idx) == null) {
+                    _tags = new HashSet<Tag>();
+                } else {
+                    _tags = mappedResults.get(idx);
+                }
+                _tags.add(tag);
+                mappedResults.put(idx, _tags);
             }
-            _tags.add(tag);
-            mappedResults.put(idx,_tags);
         }
         for (int key:mappedResults.keySet()) {
-            System.out.print(key + ": ");
+            /*System.out.print(key + ": ");
             for (Tag tag:mappedResults.get(key)) {
                 System.out.print(tag.getNum() + " ");
             }
-            System.out.println();
+            System.out.println();*/
         }
         return mappedResults;
     }
@@ -92,7 +97,12 @@ public class Client {
     public static Set<Tag> predictAdjacentTags(HashMap<Integer,Set<Tag>> expected,HashMap<Integer,Set<Tag>> reality) {
         Set<Tag> tags = new HashSet<Tag>();
         for (int key:reality.keySet()) {
-            tags.addAll(expected.get(key));
+            Set<Tag> expectedTags = expected.get(key);
+            if (expectedTags.size()==1) {
+                Tag tag = expectedTags.iterator().next();
+                //tag.setStatus(0);
+                tags.add(tag);
+            }
         }
         return tags;
     }
@@ -106,11 +116,11 @@ public class Client {
             HashMap<Integer,Set<Tag>> subMappedResults = mapTags(tags,mappedIndex);
             Set<Tag> predictTags = predictAdjacentTags(allMappedResults,subMappedResults);
             allPredictTags.put(key,predictTags);
-            System.out.print("Reader " + key + " : ");
+            /*System.out.print("Reader " + key + " : ");
             for (Tag tag:predictTags) {
                 System.out.print(tag.getNum()+" ");
             }
-            System.out.println();
+            System.out.println();*/
         }
         return allPredictTags;
     }
@@ -125,20 +135,64 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) {
+    public static int getNotIdentifiedTags(Set<Tag> tags) {
+        int number = 0;
+        for (Tag tag:tags) {
+            if (tag.getStatus()!=0) {
+                number++;
+            }
+        }
+        return number;
+    }
+
+    public static int getExpectedDistributionNumbers(HashMap<Integer,Set<Tag>> distribution) {
+        int expectedNumbers = 0;
+        for (int key:distribution.keySet()) {
+            expectedNumbers += distribution.get(key).size();
+        }
+        return expectedNumbers;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         Set<Tag> tags = initTags();
         Set<Reader> readers = initReader();
         HashMap<Integer,Set<Tag>> distribution = initDistribution(readers,tags);
-        HashMap<Integer,Integer> mappedIndex = getTagMappedIndex(tags, tag_number,2015);
-        HashMap<Integer,Set<Tag>> allMappedResults = mapTags(tags,mappedIndex);
-        HashMap<Integer,Set<Tag>> allPredictTags = mapReaders(distribution,allMappedResults,mappedIndex);
-        for (int key:mappedIndex.keySet()) {
-            System.out.println(mappedIndex.get(key) + ":" +key);
+        int expectedNumbers = getExpectedDistributionNumbers(distribution);
+        int realityNumbers = 0,round = 1;
+        System.out.println("expectedNumbers: " + expectedNumbers);
+        HashMap<Integer,Set<Tag>> finalPredictTags = new HashMap<Integer, Set<Tag>>();
+        double time = 0;
+        while (realityNumbers!=expectedNumbers) {
+            System.out.println("round "+round);
+            HashMap<Integer,Integer> mappedIndex = getTagMappedIndex(tags,getNotIdentifiedTags(tags),round);
+            HashMap<Integer,Set<Tag>> allMappedResults = mapTags(tags,mappedIndex);
+            HashMap<Integer,Set<Tag>> allPredictTags = mapReaders(distribution,allMappedResults,mappedIndex);
+
+            /*for (int key:mappedIndex.keySet()) {
+                System.out.println(mappedIndex.get(key) + ":" +key);
+            }*/
+            for (int key:allPredictTags.keySet()) {
+                for (Tag tag:allPredictTags.get(key)) {
+                    tag.setStatus(0);
+                    realityNumbers++;
+                }
+
+                if (finalPredictTags.containsKey(key)) {
+                    finalPredictTags.get(key).addAll(allPredictTags.get(key));
+                } else {
+                    finalPredictTags.put(key,allPredictTags.get(key));
+                }
+            }
+
+            //output(distribution);
+            System.out.println("predict");
+            output(allPredictTags);
+            System.out.println("total identified tags: " + realityNumbers);
+            round++;
+            Thread.sleep(1000);
         }
-        System.out.println("expected");
-        output(distribution);
-        System.out.println("predict");
-        output(allPredictTags);
+        System.out.println("final predict tags:");
+        output(finalPredictTags);
     }
 
 }
